ce7f6f825b17f13b6bc727d3457816d0e6934979
Songhan Yu <songhan@macbook-pro.myfiosgateway.com>
2022-08-20 21:43:30 -0400

    update

---
+++ interview/KSmallestPairs.java
@@ -0,0 +1,64 @@
+package interview;
+
+import java.util.*;
+
+class Cell {
+  int aIdx;
+  int bIdx;
+  public Cell(int idx1, int idx2) {
+      aIdx = idx1;
+      bIdx = idx2;
+  }
+}
+
+class KSmallestPairs {
+  public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+    PriorityQueue<Cell> minheap = new PriorityQueue<>(k, new Comparator<Cell>() {
+        public int compare(Cell c1, Cell c2) {
+            int sum1 = nums1[c1.aIdx] + nums2[c1.bIdx];
+            int sum2 = nums1[c2.aIdx] + nums2[c2.bIdx];
+            if (sum1 == sum2) {
+                return 0;
+            }
+            return sum1 < sum2 ? -1 : 1;
+        }
+    });
+    
+    List<List<Integer>> result = new ArrayList<>();
+    boolean[][] visited = new boolean[nums1.length][nums2.length];
+    visited[0][0] = true;
+    minheap.offer(new Cell(0, 0));
+    
+    for(int i = 0; i < k; i++) {
+      if (minheap.peek() == null) {
+        break;
+      }
+      Cell temp = minheap.poll();
+      System.out.printf("poll %d\n", nums1[temp.aIdx] + nums2[temp.bIdx]);
+      List<Integer> oneResult = new ArrayList<>(Arrays.asList(nums1[temp.aIdx], nums2[temp.bIdx]));
+      result.add(oneResult);
+      
+      if (temp.aIdx + 1 < nums1.length && !visited[temp.aIdx + 1][temp.bIdx]) {
+          visited[temp.aIdx + 1][temp.bIdx] = true;
+          minheap.offer(new Cell(temp.aIdx + 1, temp.bIdx));
+          System.out.printf("offer %d\n", nums1[temp.aIdx + 1] + nums2[temp.bIdx]);
+      }
+      if (temp.bIdx + 1 < nums2.length && !visited[temp.aIdx][temp.bIdx + 1]) {
+          visited[temp.aIdx][temp.bIdx + 1] = true;
+          minheap.offer(new Cell(temp.aIdx, temp.bIdx + 1));
+          System.out.printf("offer %d\n", nums1[temp.aIdx] + nums2[temp.bIdx + 1]);
+      }
+    }
+    
+    return result;
+  }
+
+  public static void main(String[] args) {
+    KSmallestPairs sol = new KSmallestPairs();
+    List<List<Integer>> res = sol.kSmallestPairs(new int[]{1, 7, 11}, new int[]{2, 4, 6}, 3);
+    System.out.println(res);
+
+    res = sol.kSmallestPairs(new int[]{1, 2}, new int[]{3}, 3);
+    System.out.println(res);
+  }
+}
